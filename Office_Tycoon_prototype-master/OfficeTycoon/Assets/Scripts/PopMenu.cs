e03b690e32e976ebf957ff2446e14241ac955f1b
ss <rasemigod@mail.bg>
2019-03-06 00:43:46 +0200

    Added PopMenu Script

---
+++ Office_Tycoon_prototype-master/OfficeTycoon/Assets/Scripts/PopMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopMenu : MonoBehaviour
+{
+    private GameObject[] stars;
+    private GameObject[] labels;
+    private GameObject canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = gameObject.transform.GetChild(0).gameObject;
+        stars = new GameObject[3];
+        foreach(Transform star in canvas.transform)
+        {
+            if (star.gameObject.name.Contains("star"))
+            {
+                if(stars[0] == null)
+                {
+                    stars[0] = star.gameObject;
+                }else if ( stars[1] == null)
+                {
+                    stars[1] = star.gameObject;
+                }else
+                {
+                    stars[2] = star.gameObject;
+                }
+            }else if (star.gameObject.name.Contains("background"))
+            {
+                star.gameObject.SetActive(true);
+            }
+        }
+
+
+        canvas.SetActive(true);
+        foreach (GameObject star in stars)
+        {
+            star.SetActive(true);
+        }
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
